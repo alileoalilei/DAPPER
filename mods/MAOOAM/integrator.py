6d63b04ee902eb88636054e8b0c4a2b99e68037b
14tondeu <maxime.tondeur@mines-paristech.fr>
2016-07-06 18:34:12 +0200

    Version 1

---
+++ mods/MAOOAM/integrator.py
@@ -0,0 +1,45 @@
+from params import ndim
+import aotensor
+import numpy as np
+from scipy.sparse import dok_matrix
+from scipy.sparse import csr_matrix
+import time
+
+tensor=aotensor.init_aotensor()
+
+
+def sparse_mul3(arr) :
+	""" Calculate for each i the sums on j,k of the product
+	tensor(i,j,k)* arr(j) * arr(k) """
+	res=np.zeros(ndim+1)
+	for i in range(1,ndim+1) :
+		X=tensor[i].nonzero()
+		for m in range(0,len(X)) :
+			j=X[0][m]
+			k=X[1][m]
+			res[i]=res[i]+tensor[i][j,k]*arr[j]*arr[k]
+	return res
+	
+def tendencies(y) :
+	""" Calculate the tendencies thanks to the product of the tensor and the vector y"""
+	return sparse_mul3(y)
+
+buf_f0=np.zeros(ndim,dtype=float)
+buf_f1=np.zeros(ndim,dtype=float)
+buf_y1=np.zeros(ndim,dtype=float)
+
+def step(y,t,dt) :
+	""" Heun method integration"""
+	buf_f0=tendencies(y)
+	buf_y1= y + dt * buf_f0
+	buf_f1=tendencies(buf_y1)
+	
+	return (y + 0.5 * (buf_f0 + buf_f1) * dt,t+dt)
+
+if __name__ == "__main__":
+	import ic
+	X=ic.X0
+	#for i in range(0,80):
+	X=sparse_mul3(X)
+	print (sparse_mul3(X))
+
---
+++ mods/MAOOAM/maooam.py
@@ -0,0 +1,65 @@
+import numpy as np
+import params
+import aotensor
+import integrator
+from plot import *
+import time
+import ic_def
+
+class bcolors:
+#to color the instructions in the console
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+print (bcolors.OKBLUE + "MODEL MAOOAM" + bcolors.ENDC)
+print (bcolors.OKBLUE + "Initialization" + bcolors.ENDC)
+
+ic_def.load_IC()
+import ic
+X= ic.X0
+
+print (bcolors.OKBLUE + "Starting the transient time evolution..." + bcolors.ENDC)
+
+t=0.
+T=time.clock()
+while(t<params.t_trans) :
+	print (t)
+	(X,t)=integrator.step(X,t,params.dt)
+
+print (bcolors.OKBLUE + "Starting the  time evolution..." + bcolors.ENDC)
+#we write in the file on this section
+#ndim+1 columns : 1 for time and one for the ndim coordinates
+fichier = open("evol_field_python_"+str(params.t_trans)+" "+str(params.t_run)+".dat", "w")
+t=0.
+
+while (t<params.t_run) :
+	#one step
+	print (t)
+	(X,t)=integrator.step(X,t,params.dt)
+	if(t%(params.tw) <params.dt) :
+		fichier.write(str(t)+" ")
+		for i in range(1,params.ndim+1) :
+			fichier.write(str.format("{0:.12f}", X[i])+" ")
+		fichier.write("\n")
+fichier.close()
+print (bcolors.OKBLUE + "Evolution finished "+ bcolors.ENDC)
+
+d=time.clock()-T
+
+print (bcolors.OKBLUE + "Time clock :" + bcolors.ENDC)
+print (d)
+
+print (bcolors.OKBLUE + "Calculating mean and variance" + bcolors.ENDC)
+#calculate_stat()
+
+print (bcolors.OKBLUE + "Plot thetao2 psio2 psia1" + bcolors.ENDC)
+
+#plotD()
+
